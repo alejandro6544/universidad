31680ab1a8634107df2e64d0fb5fb613f22e0303
Alejandro <alejandro6544@gmail.com>
2018-09-20 08:48:38 -0500

    Initial commit

---
+++ src/control/ConnectBD.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import java.sql.*;
+
+/**
+ *
+ * @author dacastro
+ */
+public class ConnectBD {
+
+    Connection conexion;
+    Statement st;
+
+    public ConnectBD() {
+        //conexion
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+    
+    public boolean crearConexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");                                      //user  //pass
+            conexion = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/ucompleta", "adminu", "123123");
+            st = conexion.createStatement();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+    
+
+}
